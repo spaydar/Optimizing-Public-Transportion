f70a4dbccda917a2d584a87102437dbf0aefa714
Seth Paydar <sethpaydar@sbcglobal.net>
2022-08-25 01:30:33 +0000

    fix: removed trailing comma in SQL

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -36,7 +36,7 @@ CREATE TABLE turnstile_summary
 WITH (VALUE_FORMAT='JSON') AS
     SELECT
         station_id,
-        COUNT(station_id) AS count,
+        COUNT(station_id) AS count
     FROM turnstile
     GROUP BY station_id
 """
