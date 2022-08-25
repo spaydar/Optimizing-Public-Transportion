15eb0afdc49651c78ecfe1c03bc060a617b525fa
Seth Paydar <sethpaydar@sbcglobal.net>
2022-08-25 01:23:10 +0000

    feat: ksql.py

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -13,24 +13,32 @@ logger = logging.getLogger(__name__)
 KSQL_URL = "http://localhost:8088"
 
 #
-# TODO: Complete the following KSQL statements.
-# TODO: For the first statement, create a `turnstile` table from your turnstile topic.
+# DONE: Complete the following KSQL statements.
+# DONE: For the first statement, create a `turnstile` table from your turnstile topic.
 #       Make sure to use 'avro' datatype!
-# TODO: For the second statment, create a `turnstile_summary` table by selecting from the
+# DONE: For the second statment, create a `turnstile_summary` table by selecting from the
 #       `turnstile` table and grouping on station_id.
 #       Make sure to cast the COUNT of station id to `count`
 #       Make sure to set the value format to JSON
 
 KSQL_STATEMENT = """
 CREATE TABLE turnstile (
-    ???
+    station_id INT,
+    station_name VARCHAR,
+    line VARCHAR
 ) WITH (
-    ???
+    KAFKA_TOPIC='org.chicago.cta.turnstile',
+    VALUE_FORMAT='AVRO',
+    KEY='station_id'
 );
 
 CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
+WITH (VALUE_FORMAT='JSON') AS
+    SELECT
+        station_id,
+        COUNT(station_id) AS count,
+    FROM turnstile
+    GROUP BY station_id
 """
 
 
